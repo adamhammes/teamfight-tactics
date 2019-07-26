da1ee6ce3cf022be73c5f7d8e0ad6fe68d2ee394
Adam Hammes <ahammes@cortexmedia.ca>
2019-07-26 18:21:50 -0400

    Add desktop layout for item combines

--- src/pages/items.js
+++ src/pages/items.js
@@ -1,82 +1,126 @@
 import React, { useState } from "react";
-import { Link, graphql } from "gatsby";
+import { graphql } from "gatsby";
+import { Image, useMatchMedia, useIsMounted } from "../utils/index";
 import { css } from "@emotion/core";
-import { Image } from "../utils/index";
 
-const tabList = css`
-  list-style-type: none;
-  margin: 0 1rem 0 0;
-  padding: 0;
+const ItemImage = ({ item }) => (
+  <div style={{ width: `3rem`, height: `3rem`, overflow: "hidden" }}>
+    <Image src={`item-icons/${item.key}.jpg`} />
+  </div>
+);
 
-  li {
-    display: flex;
+const MobileLayout = ({ zippedItems, setSelectedItemKey, selectedItem }) => (
+  <table style={{ fontSize: 15 }}>
+    <thead>
+      <tr>
+        <th>&nbsp;</th>
+        <th>
+          <ItemImage item={selectedItem} />
+        </th>
+        <th>{selectedItem.bonus}</th>
+      </tr>
+    </thead>
+    <tbody>
+      {zippedItems.map(([item, combined]) => (
+        <tr valign="top" key={`${item.key} - ${combined.key}`}>
+          <td onClick={() => setSelectedItemKey(item.key)}>
+            <ItemImage item={item} />
+          </td>
+          <td style={{ height: "3.5rem" }}>
+            <ItemImage item={combined} />
+          </td>
+          <td>{combined.bonus}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
 
-    > div:first-child {
-      flex-shrink: 0;
-    }
+const desktopCss = css`
+  display: flex;
+  justify-content: center;
+
+  td,
+  th {
+    padding: 0.25rem;
   }
+`;
 
-  li + li {
-    margin-top: 1rem;
-  }
-`
+const DesktopLayout = ({ basicItems, itemGrid }) => (
+  <div css={desktopCss}>
+    <table>
+      <thead>
+        <tr>
+          <th>&nbsp;</th>
+          {basicItems.map(item => (
+            <th key={item.key}>
+              <ItemImage item={item} />
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {itemGrid.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            <td>
+              <ItemImage item={basicItems[rowIndex]} />
+            </td>
+            {row.map(combinedItem => (
+              <td key={combinedItem.key}>
+                <ItemImage item={combinedItem} />
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
 
 const ItemsListing = ({ data }) => {
+  const isMounted = useIsMounted();
+  const isDesktop = useMatchMedia("(min-width: 700px)");
+
   const items = data.items.edges.map(edge => edge.node);
 
-  const basicItems = items.filter(item => item.kind === 'basic');
+  const basicItems = items.filter(item => item.kind === "basic");
   const [selectedItemKey, setSelectedItemKey] = useState(basicItems[0].key);
 
   const selectedItem = basicItems.find(item => item.key === selectedItemKey);
   const buildsInto = selectedItem.buildsInto;
 
   const zippedItems = basicItems.map(item => [item, buildsInto[item.key]]);
-  console.log(zippedItems);
 
-  return (
-    <table style={{ fontSize: 15 }}>
-      <thead>
-        <tr>
-          <td>&nbsp;</td>
-          <td>
-            <div style={{ width: `3rem`, height: `3rem`, overflow: 'hidden' }}>
-              <Image src={`item-icons/${selectedItem.key}.jpg`}/>
-            </div>
-          </td>
-          <td>
-            {selectedItem.bonus}
-          </td>
-        </tr>
-      </thead>
-      <tbody>
-        {zippedItems.map(([item, combined]) => (
-          <tr valign="top" key={`${item.key} - ${combined.key}`}>
-            <td onClick={() => setSelectedItemKey(item.key)}>
-              <div style={{ width: `3rem`, height: `3rem`, overflow: 'hidden' }}>
-                <Image src={`item-icons/${item.key}.jpg`}/>
-              </div>
-            </td>
-            <td style={{ height: '3.5rem' }}>
-              <div style={{ width: `3rem`, height: `3rem`, overflow: 'hidden' }}>
-                <Image src={`item-icons/${combined.key}.jpg`}/>
-              </div>
-            </td>
-            <td>
-              {combined.bonus}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+  const itemGrid = [];
+  for (const firstItem of basicItems) {
+    itemGrid.push([]);
+    for (const secondItem of basicItems) {
+      const currentRow = itemGrid[itemGrid.length - 1];
+      currentRow.push(firstItem.buildsInto[secondItem.key]);
+    }
+  }
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return isDesktop ? (
+    <DesktopLayout basicItems={basicItems} itemGrid={itemGrid} />
+  ) : (
+    <MobileLayout
+      zippedItems={zippedItems}
+      selectedItem={selectedItem}
+      setSelectedItemKey={setSelectedItemKey}
+    />
   );
-}
+};
 
 export default ItemsListing;
 
 export const pageQuery = graphql`
   query BasicItems {
     items: allItemsJson {
-     edges {
+      edges {
         node {
           kind
           name
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,42 +1,79 @@
-import React from 'react';
-import Img from 'gatsby-image';
-import { StaticQuery, graphql } from 'gatsby';
+import React, { useState, useEffect } from "react";
+import Img from "gatsby-image";
+import { StaticQuery, graphql } from "gatsby";
+
+// Usage:
+//
+// const isDesktop = useMatchMedia('(min-width: 1000px)');
+// if (isDesktop) { ... }
+const useMatchMedia = rawMediaQuery => {
+  const [match, setMatch] = useState(false);
+
+  useEffect(() => {
+    const matchMedia = window.matchMedia(rawMediaQuery);
+    setMatch(matchMedia.matches);
+
+    const listener = matchMedia.addListener(e => setMatch(e.matches));
+
+    return () => matchMedia.removeListener(listener);
+  }, [rawMediaQuery]);
+
+  return match;
+};
+
+const useIsMounted = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+
+    return () => setIsMounted(false);
+  }, []);
+
+  return isMounted;
+};
 
 function renderImage(file) {
-  return <Img fluid={file.node.childImageSharp.fluid} />
+  return <Img fluid={file.node.childImageSharp.fluid} />;
 }
 
-const Image = function (props) {
-  return <StaticQuery
-    query={graphql`
-      query {
-        images: allFile(filter:{ extension: { regex: "/jpeg|jpg|png|gif/"}}) {
-        edges {
-          node {
-            extension
-            relativePath
-            childImageSharp {
-              fluid(maxWidth: 1000) {
-                ...GatsbyImageSharpFluid
+const Image = function(props) {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          images: allFile(
+            filter: { extension: { regex: "/jpeg|jpg|png|gif/" } }
+          ) {
+            edges {
+              node {
+                extension
+                relativePath
+                childImageSharp {
+                  fluid(maxWidth: 1000) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
               }
             }
           }
         }
-      }
-    }
-    `}
-    render={({ images }) => {
-      const imageData = images.edges.find(image => image.node.relativePath === props.src);
-      if (imageData) {
-        return renderImage(imageData)
-      }
+      `}
+      render={({ images }) => {
+        const imageData = images.edges.find(
+          image => image.node.relativePath === props.src
+        );
+        if (imageData) {
+          return renderImage(imageData);
+        }
 
-      console.error(`Could not find image with path ${props.src}`);
-      console.log('Available images:');
-      console.log(images.edges.map(edge => edge.node.relativePath));
-      return null;
-    }}
-  />
-}
+        console.error(`Could not find image with path ${props.src}`);
+        console.log("Available images:");
+        console.log(images.edges.map(edge => edge.node.relativePath));
+        return null;
+      }}
+    />
+  );
+};
 
-export { Image };
+export { Image, useMatchMedia, useIsMounted };
