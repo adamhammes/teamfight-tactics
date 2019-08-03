a4b09c035e3db2ad05dc744864d47581e3b9203e
Adam Hammes <ahammes@cortexmedia.ca>
2019-08-03 12:53:34 -0400

    Data/python cleanup

--- data/transform.py
+++ data/transform.py
@@ -6,37 +6,37 @@ import re
 
 def slugify(s):
     s = s.lower()
-    for c in [' ', '-', '.', '/']:
-        s = s.replace(c, '_')
+    for c in [" ", "-", ".", "/"]:
+        s = s.replace(c, "_")
 
-    s = re.sub('\W', '', s)
+    s = re.sub("\W", "", s)
 
-    s = s.replace('_', ' ')
-    s = re.sub('\s+', ' ', s)
+    s = s.replace("_", " ")
+    s = re.sub("\s+", " ", s)
     s = s.strip()
-    s = s.replace(' ', '-')
+    s = s.replace(" ", "-")
 
     return s
 
 
 def champions():
-    with open('solomid/champions-backup.json') as f:
+    with open("data/solomid/champions-backup.json") as f:
         raw_champions = json.load(f, object_pairs_hook=collections.OrderedDict)
 
     champions_list = list(raw_champions.values())
 
     for champion in champions_list:
-        champion['slug'] = slugify(champion['name'])
+        champion["slug"] = slugify(champion["name"])
 
-        ability = champion['ability']
-        del ability['stats']
+        ability = champion["ability"]
+        del ability["stats"]
 
-    with open('champion-list.json', 'w') as f:
+    with open("data/champion-list.json", "w") as f:
         json.dump(champions_list, f, indent=4)
 
 
 def items():
-    with open('solomid/items-backup.json') as f:
+    with open("data/solomid/items-backup.json") as f:
         raw_items = json.load(f, object_pairs_hook=collections.OrderedDict)
 
     items = list(raw_items.values())
@@ -46,32 +46,31 @@ def items():
     for item in items:
         # copy and delete circular references
         copied_item = copy.deepcopy(item)
-        copied_item.pop('buildsInto', None)
-        copied_item.pop('buildsFrom', None)
+        copied_item.pop("buildsInto", None)
+        copied_item.pop("buildsFrom", None)
 
-        if copied_item['kind'] == 'basic':
-            basic_items[copied_item['key']] = copied_item
+        if copied_item["kind"] == "basic":
+            basic_items[copied_item["key"]] = copied_item
         else:
-            advanced_items[copied_item['key']] = copied_item
+            advanced_items[copied_item["key"]] = copied_item
 
     basic_items_keys = basic_items.keys()
 
     for item in items:
         new_builds_into = collections.OrderedDict()
         new_builds_from = []
-        if item['kind'] == 'basic':
-            builds_into = item['buildsInto']
+        if item["kind"] == "basic":
+            builds_into = item["buildsInto"]
             for basic_key, advanced_key in zip(basic_items_keys, builds_into):
                 new_builds_into[basic_key] = advanced_items[advanced_key]
 
         else:
-            new_builds_from = [basic_items[item_key] for item_key in item['buildsFrom']]
+            new_builds_from = [basic_items[item_key] for item_key in item["buildsFrom"]]
 
-        item['buildsInto'] = new_builds_into
-        item['buildsFrom'] = new_builds_from
+        item["buildsInto"] = new_builds_into
+        item["buildsFrom"] = new_builds_from
 
-
-    with open('items.json', 'w') as f:
+    with open("data/items.json", "w") as f:
         json.dump(items, f, indent=4)
 
 
---
+++ data/update-data.py
@@ -0,0 +1,23 @@
+import urllib.request
+from transform import main as run_transform
+
+json_files = {
+    "data/solomid/items-backup.json": "https://solomid-resources.s3.amazonaws.com/blitz/tft/data/items.json",
+    "data/solomid/champions-backup.json": "https://solomid-resources.s3.amazonaws.com/blitz/tft/data/champions.json",
+}
+
+
+def main():
+    print("Download json files...")
+    for file_name, url in json_files.items():
+        urllib.request.urlretrieve(url, file_name)
+        print("    * {}".format(file_name))
+
+    print("...done.\n")
+    print("Running transform.py...")
+    run_transform()
+    print("...done.\n")
+
+
+if __name__ == "__main__":
+    main()
