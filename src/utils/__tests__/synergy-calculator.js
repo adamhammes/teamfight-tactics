793f64f7f61313ffd22aa3f80f6b012d5739e7ba
Adam Hammes <ahammes@cortexmedia.ca>
2019-08-04 12:49:59 -0400

    Add ninja tests for synergy calculator

--- src/utils/__tests__/synergy-calculator.js
+++ src/utils/__tests__/synergy-calculator.js
@@ -1,5 +1,52 @@
+import champions from "../../../data/cleaned-data/champion-list.json";
+import synergies from "../../../data/cleaned-data/synergies.json";
+
+import synergyCalculator from "../synergy-calculator";
+
+const champion = championSlug => champions.find(c => c.slug == championSlug);
+
 describe("Synergy Calculator", () => {
-  it("Can pass a test", () => {
-    expect(2 + 2).toEqual(4);
+  it("works for the single ninja bonus", () => {
+    const akali = champions.find(c => c.slug == "akali");
+    const ninjaSynergy = synergies.find(s => s.key == "ninja");
+
+    const akaliSynergies = synergyCalculator(synergies, [akali]);
+
+    expect(akaliSynergies).toHaveLength(1);
+
+    const expectedSynergy = {
+      synergy: ninjaSynergy,
+      bonus: ninjaSynergy.bonuses[0]
+    };
+
+    expect(akaliSynergies[0]).toEqual(expectedSynergy);
+  });
+
+  it("does not give the ninja bonus for two ninjas", () => {
+    const calculatedSynergies = synergyCalculator(synergies, [
+      champion("akali"),
+      champion("zed")
+    ]);
+
+    expect(calculatedSynergies).toHaveLength(0);
+  });
+
+  it("gives ninja bonus to 4 ninjas", () => {
+    const ninjaSynergy = synergies.find(s => s.key == "ninja");
+    const calculatedSynergies = synergyCalculator(synergies, [
+      champion("akali"),
+      champion("zed"),
+      champion("shen"),
+      champion("kennen")
+    ]);
+
+    expect(calculatedSynergies).toHaveLength(1);
+
+    const expectedReturn = {
+      synergy: ninjaSynergy,
+      bonus: ninjaSynergy.bonuses[1]
+    };
+
+    expect(calculatedSynergies[0]).toEqual(expectedReturn);
   });
 });
