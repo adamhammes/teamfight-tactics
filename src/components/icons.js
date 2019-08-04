d01728d7d45f495e778d0de6e183ea6eff92343c
Adam Hammes <ahammes@cortexmedia.ca>
2019-08-04 11:58:46 -0400

    Remove unnecessary return

--- src/components/icons.js
+++ src/components/icons.js
@@ -1,21 +1,19 @@
 import React from "react";
 
-export const IconXCircle = ({ size = 24, ...props }) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      width={size}
-      height={size}
-      {...props}
-    >
-      <path
-        className="heroicon-ui"
-        d="M4.93 19.07A10 10 0 1 1 19.07 4.93 10 10 0 0 1 4.93 19.07zm1.41-1.41A8 8 0 1 0 17.66 6.34 8 8 0 0 0 6.34 17.66zM13.41 12l1.42 1.41a1 1 0 1 1-1.42 1.42L12 13.4l-1.41 1.42a1 1 0 1 1-1.42-1.42L10.6 12l-1.42-1.41a1 1 0 1 1 1.42-1.42L12 10.6l1.41-1.42a1 1 0 1 1 1.42 1.42L13.4 12z"
-      />
-    </svg>
-  );
-};
+export const IconXCircle = ({ size = 24, ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={size}
+    height={size}
+    {...props}
+  >
+    <path
+      className="heroicon-ui"
+      d="M4.93 19.07A10 10 0 1 1 19.07 4.93 10 10 0 0 1 4.93 19.07zm1.41-1.41A8 8 0 1 0 17.66 6.34 8 8 0 0 0 6.34 17.66zM13.41 12l1.42 1.41a1 1 0 1 1-1.42 1.42L12 13.4l-1.41 1.42a1 1 0 1 1-1.42-1.42L10.6 12l-1.42-1.41a1 1 0 1 1 1.42-1.42L12 10.6l1.41-1.42a1 1 0 1 1 1.42 1.42L13.4 12z"
+    />
+  </svg>
+);
 
 export const IconTrash = ({ size = 24, ...props }) => (
   <svg
